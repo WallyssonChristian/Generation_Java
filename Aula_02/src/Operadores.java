46efb85274e640d6181bbe8dfcaf249aae43c97a
Wally <cerberus74@live.com>
2023-11-13 12:27:11 -0300

    Aula 02 - Operadores

--- Aula_02/src/Operadores.java
+++ Aula_02/src/Operadores.java
@@ -5,7 +5,7 @@ public class Operadores {
 	static Scanner leia = new Scanner(System.in);
 	public static void main(String[] args) {
 		int num1 = 7, num2 = 4;
-		String v1 = "2", v2 = "3";
+		String var1 = "2", var2 = "3";
 		
 		String nome = "Wally";
 		int idade = 26;
@@ -14,7 +14,7 @@ public class Operadores {
 		int tamanho;
 		
 		System.out.println("A soma das variáveis numero1 e numero2 é igual a: " + (num1+num2));
-		System.out.println("A soma das variáveis valor1 e valor2 é igual a: " + (v1+v2));
+		System.out.println("A soma das variáveis valor1 e valor2 é igual a: " + (var1+var2));
 		
 		System.out.println("meu nome é " + nome + " e eu tenho " + idade + " anos.");
 		
